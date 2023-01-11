1d549aba348ea1c6b7e4e4cf1457dc9d0e54d6e7
Dorian Mayamba <dorian.mayamba@gmail.com>
2023-01-11 04:09:26 +0000

    updated the service page and added role, and user seeders

--- resources/views/Services.blade.php
+++ resources/views/Services.blade.php
@@ -10,19 +10,25 @@
             <div class="service1 col-md-4">
                 <h2 class="text-center"><small>Desktop applications</small></h2>
                 <p>
-
+                    The purpose of these applications is to be run in a desktop computer.
+                    They are mostly developed in c#, java and python. The goal of the applications is to
+                    allow the users to automate redundant desktop tasks.
                 </p>
             </div>
             <div class="service2 col-md-4">
                 <h2 class="text-center"><small>Mobile applications</small></h2>
                 <p>
-
+                    The purpose of these applications is to be run in the mobile devices. the goal
+                    of these applications is to allow the users that cannot access a desktop device to 
+                    automate their tasks from a mobile device. They will be able to access the same service as any desktop
+                    application.
                 </p>
             </div>
             <div class="service3 col-md-4">
                 <h2 class="text-center"><small>Web applications</small></h2>
                 <p>
-
+                    Web Applications will allow the users to manage their businesses online. They are often 
+                    to manage sales etc...
                 </p>
             </div>
         </div>
